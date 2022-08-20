51404b3f06f458096da30c75033b40eb65cb056f
Bilal Younes <bilalyounes1999@gmail.com>
2022-08-20 11:48:48 +0300

    second commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom'
 import './Navbar.css'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
         <div className="navContiner">
+          <Link to={"/"}>
             <span className="logo">YounBooking</span>
+            </Link>
             <div className="navItems">
                <button className="navButton">Register</button>
                 <button className="navButton">Login</button>
